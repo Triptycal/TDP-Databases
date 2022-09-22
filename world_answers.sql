6d6a49ccf4e3e85c9e6605c07e345adf74d0d1c6
Triptycal <36767994+Triptycal@users.noreply.github.com>
2022-09-22 16:00:37 +0100

    Add files via upload

--- world_answers.sql
+++ world_answers.sql
@@ -1,23 +1,27 @@
-1.	Using COUNT, get the number of cities in the USA.
- 274
+#1.	Using COUNT, get the number of cities in the USA.
+ #274
  SELECT COUNT(Name) FROM city WHERE CountryCode = "USA"; #(READ - DML)
-2.	Find out the population and life expectancy for people in Argentina.
-Population: 37032000  Life Expectancy: 75.1
+#2.	Find out the population and life expectancy for people in Argentina.
+#Population: 37032000  Life Expectancy: 75.1
 SELECT Population,LifeExpectancy FROM country WHERE Name = "Argentina"; #(READ - DML)
-3.	Using IS NOT NULL, ORDER BY, and LIMIT, which country has the highest life expectancy?
-83.5	Andorra
+#3.	Using IS NOT NULL, ORDER BY, and LIMIT, which country has the highest life expectancy?
+#83.5	Andorra
 SELECT LifeExpectancy,Name FROM country WHERE LifeExpectancy is NOT NULL ORDER BY 
 LifeExpectancy DESC limit 1;
-4.	Using JOIN ... ON, find the capital city of Spain.
-Madrid
+#4.	Using JOIN ... ON, find the capital city of Spain.
+#Madrid
 SELECT City.Name FROM country JOIN city ON country.Capital=city.ID WHERE country.name="Spain";
-5.	Using JOIN ... ON, list all the languages spoken in the Southeast Asia region.
-47 languages
+#5.	Using JOIN ... ON, list all the languages spoken in the Southeast Asia region.
+#47 languages
 SELECT DISTINCT countrylanguage.Language FROM countrylanguage JOIN country ON countrylanguage.CountryCode=country.Code WHERE country.Region="Southeast Asia";
 
-6.	Using a single query, list 25 cities around the world that start with the letter F.
-7.	Using COUNT and JOIN ... ON, get the number of cities in China.
-8.	Using IS NOT NULL, ORDER BY, and LIMIT, which country has the lowest population? Discard non-zero populations.
+#6.	Using a single query, list 25 cities around the world that start with the letter F.
+SELECT * FROM city WHERE name LIKE "f%" LIMIT 25;
+#7.	Using COUNT and JOIN ... ON, get the number of cities in China.
+#363
+
+SELECT COUNT(city.name) FROM city JOIN Country ON city.CountryCode=Country.Code WHERE Country.Name="China";
+#8.	Using IS NOT NULL, ORDER BY, and LIMIT, which country has the lowest population? Discard non-zero populations.
 9.	Using aggregate functions, return the number of countries the database contains.
 10.	What are the top ten largest countries by area?
 11.	List the five largest cities by population in Japan.
